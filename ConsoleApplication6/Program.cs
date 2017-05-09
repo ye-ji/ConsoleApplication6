f3fad7b1beaeeeecf9ccb3f8873e0dddd2594e68
yj.lee <ygee0613@gmail.com>
2017-05-09 11:25:14 +0900

    소멸자

--- ConsoleApplication6/Program.cs
+++ ConsoleApplication6/Program.cs
@@ -6,10 +6,35 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication6
 {
+    class MyClass
+    {
+        string Message;
+
+        public MyClass()
+        {
+            Message = "Hello World";
+        }
+
+        public void PrintMessage()
+        {
+            Console.WriteLine(Message);
+        }
+
+        // 소멸자
+        ~MyClass()
+        {
+            Console.Write("소멸자 호출");
+        }
+    }
+
+
     class Program
     {
         static void Main(string[] args)
         {
+            MyClass MyTest = new MyClass();
+            // MyTest.PrintMessage();
+
         }
     }
 }
